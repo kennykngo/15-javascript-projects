935476cef064b983bc2e9de65afbabb6dcb5b51b
Kenny Ngo <kenny.ngo.design@gmail.com>
2020-10-21 16:20:47 -0700

    added another method 7. for specificity

--- 7-questions/setup/app.js
+++ 7-questions/setup/app.js
@@ -1,10 +1,21 @@
  //using selectors inside the element
 // traversing the dom
-const btns = document.querySelectorAll(".question-btn");
 
-btns.forEach(function (btn) {
-	btn.addEventListener("click", function (e) {
-		const question  =e.currentTarget.parentElement.parentElement;
-		question.classList.toggle("show-text");
+const questions = document.querySelectorAll(".question");
+// console.log(questions);
+questions.forEach(function (question) { 
+	// console.log(question);
+	const btn = question.querySelector(".question-btn");
+	btn.addEventListener("click", function () {
+		question.classList.toggle("show-text")
 	})
 })
+
+// const btns = document.querySelectorAll(".question-btn");
+
+// btns.forEach(function (btn) {
+// 	btn.addEventListener("click", function (e) {
+// 		const question  =e.currentTarget.parentElement.parentElement;
+// 		question.classList.toggle("show-text");
+// 	})
+// })
