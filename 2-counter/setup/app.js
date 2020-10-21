8ec2c3430a8a7bc05493e29c1df7646a01d19daf
Kenny Ngo <kenny.ngo.design@gmail.com>
2020-10-20 22:51:03 -0700

    working on counter

---
+++ 2-counter/setup/app.js
@@ -0,0 +1,17 @@
+// setting intitial count
+let count = 0;
+
+// select value and buttons
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function (btn) { 
+	btn.addEventListener("click", function (e) { 
+		const styles = e.currentTarget.classList;
+
+		if (styles.contains("decrease")) {
+			count--;
+		} 
+		value.textContent = count;
+	})
+})
