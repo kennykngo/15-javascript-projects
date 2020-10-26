434059e31704cdc8aa8ad1bc35bfc0b118bd0f3d
Kenny Ngo <kenny.ngo.design@gmail.com>
2020-10-26 12:28:42 -0700

    Dynamically setup date on project 12

--- 12-countdown-timer/setup/app.js
+++ 12-countdown-timer/setup/app.js
@@ -21,3 +21,21 @@ const weekdays = [
   "Friday",
   "Saturday",
 ];
+
+const giveaway = document.querySelector(".giveaway");
+const deadline = document.querySelector(".deadline");
+const items = document.querySelectorAll(".deadline-format h4");
+
+let futureDate = new Date(2020, 4, 24, 11, 30, 0);
+console.log(futureDate);
+
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+
+let month = futureDate.getMonth();
+month = months[month];
+const date = futureDate.getDate();
+console.log(date);
+
+giveaway.textContent = `giveaway ends on ${month} ${date}, ${year} ${hours}:${minutes}am `;
